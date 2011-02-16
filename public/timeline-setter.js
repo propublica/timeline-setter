5680dd0de775da4f6c1820770a5942aae64b681d
Al Shaw <almshaw@gmail.com>
2011-02-16 15:39:56 -0500

    working all js version

--- public/timeline-setter.js
+++ public/timeline-setter.js
@@ -1,32 +1,43 @@
-function TimelineSetter() {
-  this.items = [];
-  
-  var that = this;
-  $(".item").each(function() {
-    that.items.push(Number($(this).attr("data-item-timestamp")));
-  });
-  this.max = _(this.items).max();
-  this.min = _(this.items).min();
+function TimelineSetter(timelineData) {
+  this.items = timelineData;
+  this.times = _(this.items).map(function(q){
+    return q.timestamp;
+  })
+  this.max = _(this.times).max();
+  this.min = _(this.times).min();
 }
 
-TimelineSetter.prototype.positionItems = function() {
+TimelineSetter.prototype.createNotches = function() {
+  console.log("max: "+ this.max);
+  console.log("min: "+ this.min);
   var that = this;
   _(this.items).each(function(item) {
-    var timestamp = item;
-    var position = ((that.max / that.min) - (that.max / item)) * 1000;
-    var el = $("#item_" + timestamp);
-    el.css("left", position + "%");
-    if (position > 70) {
-      el.addClass("expand_left");
-    }
+    var timestamp = item.timestamp;
+    var position = ((that.max - timestamp) / (that.max - that.min)) * 100;
+    $(".timeline_notchbar").append(
+      "<div class='timeline_notch notch_" + timestamp + "' data-timestamp='" + timestamp + "'>&nbsp;</div>"
+    );
+    $(".notch_" + timestamp).css("left",position + "%");
   });
-  return this;
+}
+
+TimelineSetter.prototype.template = function(timestamp) {
+  var item,tmpl,html;
+  item = _(this.items).select(function(q) {
+    return q.timestamp === Number(timestamp);
+  })[0];
+  tmpl = _.template($("#card_tmpl").html());
+  html = tmpl(item);
+  return html; 
 }
 
 $(document).ready(function() {
-  var timeline = new TimelineSetter();
-  timeline.positionItems();
+  var page_timeline = new TimelineSetter(timelineData);
+  page_timeline.createNotches();
+  $(".timeline_notch").hover(function() {
+    var timestamp = $(this).attr("data-timestamp");
+    var html = page_timeline.template(timestamp)
+    $("#timeline_card_container").html(html).attr("style",$(this).attr("style"))
+  },function() {
+  });
 });
-
-
-/////////////////////////////////////
