8dbba168fbc7756471c6a666a7b41d1721fd12c5
Al Shaw <almshaw@gmail.com>
2011-02-16 13:32:08 -0500

    organization, take templates out of lib, generate json

---
+++ lib/timeline_setter.rb
@@ -0,0 +1,63 @@
+require 'date'
+require 'time'
+require 'erb'
+require 'rubygems'
+require 'table_fu'
+require 'json'
+
+module TimelineSetter
+  class Parser
+    attr_reader :events
+    
+    # sheet should contain columns for
+    #  * event_date
+    #  * event_display_date
+    #  * event_description
+    #  * event_link
+    #  * event_thumbnail
+    def initialize sheet
+      @events = []
+      spreadsheet = TableFu.new(sheet) do |s|
+        s.columns = %w[event_date event_display_date event_description event_link event_thumbnail]
+      end
+      
+      spreadsheet.rows.each do |row|
+        hash = spreadsheet.columns.inject({}) {|memo, column| memo[column.to_sym] = row[column].to_s ; memo}
+        @events << hash
+      end
+    end
+  end
+  
+  class Event
+    attr_reader :data
+    def initialize(evt = {})
+      @data = evt
+    end
+        
+    def to_html
+      ERB.new(File.open(File.expand_path("../../templates/event.erb", __FILE__)).read).result(binding)
+    end
+  end
+  
+  class Timeline
+    attr_reader :timeline
+    def initialize(events)
+      @events = events
+    end
+    
+    def to_html
+      @events.inject([]) {|memo, evt| memo << Event.new(evt).to_html ; memo }
+    end
+    
+    def to_json
+      @events.collect {|r| r[:timestamp] = Time.parse(r[:event_date]).to_i }
+      @events.to_json
+    end
+        
+    def timeline
+      @html = self.to_html
+      @timeline = ERB.new(File.open(File.expand_path("../../templates/timeline.erb", __FILE__)).read).result(binding)
+    end
+  end
+
+end
--- public/timeline-setter.js
+++ public/timeline-setter.js
@@ -10,17 +10,17 @@ function TimelineSetter() {
 }
 
 TimelineSetter.prototype.positionItems = function() {
-    var that = this;
-    _(this.items).each(function(item) {
-      var timestamp = item;
-      var position = ((that.max / that.min) - (that.max / item)) * 1000;
-      var el = $("#item_" + timestamp);
-      el.css("left", position + "%");
-      if (position > 70) {
-        el.addClass("expand_left");
-      }
-    });
-    return this;
+  var that = this;
+  _(this.items).each(function(item) {
+    var timestamp = item;
+    var position = ((that.max / that.min) - (that.max / item)) * 1000;
+    var el = $("#item_" + timestamp);
+    el.css("left", position + "%");
+    if (position > 70) {
+      el.addClass("expand_left");
+    }
+  });
+  return this;
 }
 
 $(document).ready(function() {
@@ -30,92 +30,3 @@ $(document).ready(function() {
 
 
 /////////////////////////////////////
-
-function DisabilityTimelineCard(el,e) {
-  this.e = e;
-  this.el = $(el);
-  this.initialWidth = this.el.width();
-  this.height = this.el.height();
-  this.label = this.el.children(".item_label");
-  this.doc_img = this.el.children(".doc_img");
-};
-
-DisabilityTimelineCard.prototype.activate = function() {
-  this.e.type === "mouseenter" ? this.el.addClass("item_active") : this.el.removeClass("item_active")
-  return this;
-}
-
-DisabilityTimelineCard.prototype.toFront = function() {
-  var cards = $(".item");
-  var card = this.el;
-  var i = _.indexOf(cards,card);
-  cards.splice(i,1);
-  cards.push(card);
-  _(cards).each(function(v,k){
-      $(v).css("z-index",k*10);
-  });
-};
-
-DisabilityTimelineCard.prototype.animation = function() {
-  var obj = {};
-  if (this.doesNotExpand()) return obj;
-  
-  var dir = this.e.type === 'mouseenter' ? '+' : '-';
-  var oppdir = dir === "+" ? "-" : "+";
-  obj['width'] = dir + "=130px";
-  // obj['height'] = dir + "=15"
-  if (this.el.hasClass("expand_left")) {
-    obj['left'] = oppdir + "=13.5%";
-  }
-  return obj;
-};
-
-DisabilityTimelineCard.prototype.midLabelHover = function() {
-  var targetLabel,toDo;
-  var that = this;
-  targetLabel = (function() {
-    return that.el.hasClass("item_upper") ? "dept_ed_label" : "tina_brooks_label";
-  })();
-  toDo = (function() {
-    return that.e.type === "mouseenter" ? "add" : "remove";
-  })();
-  $("." + targetLabel)[toDo + 'Class'](targetLabel + "_active");
-  return this;
-};
-
-DisabilityTimelineCard.prototype.showHideDoc = function() {
-  this.e.type === "mouseenter" ? this.doc_img.show() : this.doc_img.hide();
-  return this;
-}
-
-DisabilityTimelineCard.prototype.isActive = function() {
-  return this.el.hasClass("item_active") ? true : false
-}
-
-DisabilityTimelineCard.prototype.doesNotExpand = function() {
-  return this.el.hasClass("item_no_expand") ? true : false
-}
-  
-DisabilityTimelineCard.prototype.isArrowHover = function() {
-  return this.e.target.className.match(/css_arrow/) ? true : false;
-}
-  
-/* -- */
-  
-$(function() {    
-  $(".item").hover(function(e) {
-    var item = new DisabilityTimelineCard(this,e);
-    item.toFront();
-    $(item.label).css("width", item.initialWidth);
-    $(this).addClass("item_active")
-       .animate(item.animation(), 150,function() {
-         item.showHideDoc().midLabelHover();
-       })
-  },function(e) {
-    var item = new DisabilityTimelineCard(this,e);
-    item.showHideDoc();      
-    $(this).animate(item.animation(), 150, function() {
-       item.showHideDoc().activate().midLabelHover();
-    })      
-  });
-});
