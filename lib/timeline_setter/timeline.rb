0a8cf8479cd75b7aa8d2edbeb1311de0f21866fb
Al Shaw <almshaw@gmail.com>
2011-03-28 17:39:08 -0400

    css minifying. cause regexes are fun, i guess

--- lib/timeline_setter/timeline.rb
+++ lib/timeline_setter/timeline.rb
@@ -10,9 +10,25 @@ module TimelineSetter
       @events.to_json
     end
     
+    def self.minify_css(css)
+      # take out new lines
+      css.gsub!(/\n/,'')
+
+      # take out spaces between selectors and rule blocks
+      css.gsub!(/([\.a-zA-Z_\-\#]+?)\s*\{\s*([^\{\}\s])?/,'\1{\2')
+
+      # take out spaces between rule block endings and selectors
+      css.gsub!(/\}\s*([\.a-zA-Z_\-\#]+?)/,'}\1')
+
+      # #take out spaces between rules
+      css.gsub!(/(:|;)(\s+)?([\.a-zA-Z_0-9]+)?/,'\1\3')
+
+      css
+    end
+    
     def timeline_min
       js = ""
-      css = File.open("#{TimelineSetter::ROOT}/public/stylesheets/timeline-setter.css").read
+      css = Timeline.minify_css(File.open("#{TimelineSetter::ROOT}/public/stylesheets/timeline-setter.css").read)
       libs = Dir.glob("#{TimelineSetter::ROOT}/public/javascripts/vendor/**")
       libs.each do |lib| ; js << File.open(lib,'r').read ; end
       js  << Closure::Compiler.new.compile(File.open("#{TimelineSetter::ROOT}/public/javascripts/timeline-setter.js", 'r'))
