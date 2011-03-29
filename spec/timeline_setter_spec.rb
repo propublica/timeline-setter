d33f9b814a7797ac289f844d0a843332696a99a3
Al Shaw <almshaw@gmail.com>
2011-03-29 10:21:44 -0400

    fix tests

--- spec/timeline_setter_spec.rb
+++ spec/timeline_setter_spec.rb
@@ -2,19 +2,19 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "TimelineSetter" do
   it 'should build a hash from a spreadsheet' do
-    TimelineSetter::Parser.new(TEST_CSV).events[0][:event_date].should eql("Feb. 18, 2003")
+    TimelineSetter::Parser.new(TEST_CSV).events[0][:date].should eql("Feb. 18, 2003")
   end
   
   it 'should create json from an events hash' do
     events = TimelineSetter::Parser.new(TEST_CSV)
     j = TimelineSetter::Timeline.new(events.events).to_json
-    JSON.parse(j)[0]['event_date'].should eql "Feb. 18, 2003"
+    JSON.parse(j)[0]['date'].should eql "Feb. 18, 2003"
   end
   
   it 'should create html from an events hash' do
     events = TimelineSetter::Parser.new(TEST_CSV)
     html = TimelineSetter::Timeline.new(events.events).timeline.to_s
-    html.should =~ /<h2 class=\\\"timeline-img-hed\\\">Savelkoul Joins the Army<\/h2>/
+    html.should =~ /"timestamp":1045544400/
   end
   
   it 'should create minified timelines' do
@@ -26,11 +26,11 @@ describe "TimelineSetter" do
     # test CSS
     html.should =~ /#timeline_setter/
     # test timeline
-    html.should =~ /<h2 class=\\\"timeline-img-hed\\\">Savelkoul Joins the Army<\/h2>/
+    html.should =~ /"timestamp":1045544400/
     # test underscore
-    html.should =~ /Underscore\.js 1\.1\.4/
+    html.should =~ /Underscore\.js/
     # test jQuery
-    html.should =~ /jQuery JavaScript Library v1\.4\.4/
+    html.should =~ /jQuery JavaScript Library/
     # test timeline-setter.js
     html.should =~ /INTERVAL_ORDER/
   end
