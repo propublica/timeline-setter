49f07beb86c33941c37afc815b7c53963dec85df
Al Shaw <almshaw@gmail.com>
2012-04-05 12:34:06 -0400

    only copy minified versions of js libs in minified option

--- lib/timeline_setter/timeline.rb
+++ lib/timeline_setter/timeline.rb
@@ -38,7 +38,7 @@ module TimelineSetter
     def timeline_min
       @js = ""
       @css = Kompress::CSS.new(File.open("#{TimelineSetter::ROOT}/public/stylesheets/timeline-setter.css").read).css
-      libs = Dir.glob("#{TimelineSetter::ROOT}/public/javascripts/vendor/**")
+      libs = Dir.glob("#{TimelineSetter::ROOT}/public/javascripts/vendor/**").select {|q| q =~ /min/ }
       libs.each { |lib| @js << File.open(lib,'r').read }
       @min_html = Kompress::HTML.new(timeline_markup).html
       @js << File.open("#{TimelineSetter::ROOT}/public/javascripts/timeline-setter.min.js", 'r').read
